7a60e430b9ead7fea80484d08af44573baeca1c1
Alan <asalzano@ramapo.edu>
2023-08-09 18:14:49 -0400

    added add route

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.get('/data', async (req, res) => {
 
 
 const mainRouter = require('./routes/main');
-const checkRouter = require('./routes/check');
+const checkRouter = require('./routes/add');
 const uploadRouter = require('./routes/uploadinfo');
 const viewRouter = require('./routes/viewdata');
 
